e60bf435f77e1324f9faa1aa239e3537792ca71b
CynthiaWairimu <wairimucee@gmail.com>
2022-03-24 02:18:44 +0300

    modify directory

--- js/scripts.js
+++ js/scripts.js
@@ -32,5 +32,47 @@ $(document).ready(function() {
             document.getElementById("appointment").innerHTML = ("Your pet grooming appointment is set for " + inputedDate + " at " + inputedTime + " Hours. Your cost is " + petGroom)
         }
 
+
     });
+    
 });
+
+// Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    smartSpeed: 2000,
+    dots: true,
+    loop: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+
+
