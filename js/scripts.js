dc134be4891118f86a05be31f7ef5260fae59ac3
CynthiaWairimu <wairimucee@gmail.com>
2022-03-24 14:46:43 +0300

    correct changes.

--- js/scripts.js
+++ js/scripts.js
@@ -60,18 +60,6 @@ $(".testimonial-carousel").owlCarousel({
     }
 });
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
 
 
